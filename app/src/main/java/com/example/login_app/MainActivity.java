5fa57707c9c3e261f17278dfe3c2e238a21d680f
ASUS <tdilrangi201@gmail.com>
2022-08-22 06:40:02 +0530

    KDU-App

--- app/src/main/java/com/example/login_app/MainActivity.java
+++ app/src/main/java/com/example/login_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.login_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,6 +11,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText eEmail;
     private EditText ePassword;
@@ -18,7 +24,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String password;
 
     boolean isValid = false;
+    private Button registerButton;
     private Button signup;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +37,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         signup=(Button) findViewById(R.id.signup);
         signup.setOnClickListener(this);
 
-        eEmail = findViewById(R.id.editTextTextEmailAddress);
-        ePassword = findViewById(R.id.editTextTextPassword);
-        Button eLogin = findViewById(R.id.registerButton);
+        //actually login button
+        registerButton=(Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(this);
+
+        eEmail=(EditText)findViewById(R.id.editTextTextEmailAddress);
+        ePassword=(EditText)findViewById(R.id.editTextTextPassword);
+
+        mAuth=FirebaseAuth.getInstance();
+        //eEmail = findViewById(R.id.editTextTextEmailAddress);
+        //ePassword = findViewById(R.id.editTextTextPassword);
+        //Button eLogin = findViewById(R.id.registerButton);
 
 
-        eLogin.setOnClickListener(view -> {
+        /*eLogin.setOnClickListener(view -> {
             String inputEmail = eEmail.getText().toString().trim();
             String inputPassword = ePassword.getText().toString().trim();
            /* if(inputEmail.isEmpty()){
@@ -50,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 eEmail.setError("Email is required!");
                 eEmail.requestFocus();
                 return;
-            }*/
+            }
 
 
             if (inputEmail.equals("admin") && inputPassword.equals("admin123")) {
@@ -65,7 +82,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(MainActivity.this, "Incorrect Email or Password", Toast.LENGTH_SHORT).show();
             }
 
-        });
+        });*/
 
         //ERROR!
     }
@@ -76,6 +93,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.signup:
                 startActivity(new Intent(this,Registerr.class));
             break;
+            case R.id.registerButton:
+                userLogin();
+                break;
         }
+    }
+
+    private void userLogin() {
+        String email= eEmail.getText().toString().trim();
+        String password=ePassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            eEmail.setError("Email is required!");
+            eEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            eEmail.setError("Please enter a valid email!");
+            eEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            eEmail.setError("Password is required!");
+            eEmail.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            ePassword.setError("Minimum password length should be 6 characters");
+            ePassword.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(MainActivity.this,HomePage.class));
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"Failed to login!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
